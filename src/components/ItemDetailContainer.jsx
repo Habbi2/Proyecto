16001fcc19be6b73b285b7e264f571e6f4acc1b9
Habbi413 <javieralbertoni@gmail.com>
2021-07-24 00:58:49 -0300

    v1.24

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import { useParams } from 'react-router';
+import {useParams} from 'react-router';
 import tetris from '../img/tetris.png'
 import roguelike from '../img/roguelike.png'
 import infiniteRunner from '../img/infinite-runner.png'
@@ -8,7 +8,7 @@ import ItemDetail from "./ItemDetail";
 const ItemDetailContainer = () => {
     const [itemDetails,
         setItemDetails] = useState([]);
-    const { id } = useParams();
+    const {id} = useParams();
     useEffect(() => {
         const details = [
             {
@@ -16,7 +16,10 @@ const ItemDetailContainer = () => {
                 title: "Tetris",
                 price: "150$",
                 description: "A simple Tetris",
-                text: "Tetris, video game created by Russian designer Alexey Pajitnov in 1985 that allows players to rotate falling blocks strategically to clear levels. ... The goal of the game is to prevent the blocks from stacking up to the top of the screen for as long as possible.",
+                text: "Tetris, video game created by Russian designer Alexey Pajitnov in 1985 that allo" +
+                        "ws players to rotate falling blocks strategically to clear levels. ... The goal " +
+                        "of the game is to prevent the blocks from stacking up to the top of the screen f" +
+                        "or as long as possible.",
                 pictureUrl: tetris,
                 stock: 3
             }, {
@@ -24,7 +27,9 @@ const ItemDetailContainer = () => {
                 title: "Roguelike",
                 price: "200$",
                 description: "A simple Roguelike",
-                text: "Roguelike (or rogue-like) is a subgenre of role-playing video games characterized by a dungeon crawl through procedurally generated levels, turn-based gameplay, grid-based movement, and permanent death of the player character.",
+                text: "Roguelike (or rogue-like) is a subgenre of role-playing video games characterize" +
+                        "d by a dungeon crawl through procedurally generated levels, turn-based gameplay," +
+                        " grid-based movement, and permanent death of the player character.",
                 pictureUrl: roguelike,
                 stock: 5
             }, {
@@ -32,7 +37,8 @@ const ItemDetailContainer = () => {
                 title: "Infinite-Runner",
                 price: "100$",
                 description: "A simple Infinite-Runner",
-                text: "A style of platform game in which the player character runs non-stop through a hazardous course, and must be guided to jump, duck, etc. to avoid the hazards.",
+                text: "A style of platform game in which the player character runs non-stop through a h" +
+                        "azardous course, and must be guided to jump, duck, etc. to avoid the hazards.",
                 pictureUrl: infiniteRunner,
                 stock: 7
             }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,33 @@
-import { Navbar, NavbarBrand, NavDropdown, NavLink } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import {Navbar, NavbarBrand, NavDropdown, NavLink} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 import CartWidget from './CartWidget';
 import '../components/scss/custom.scss';
- 
+
 const NavBar = () => {
     const categories = ["Adventure", "Puzzle"]
-    const style = {display: 'flex'}
+    const style = {
+        display: 'flex'
+    }
     return (
         <Navbar className="col-12" bg="dark" variant='dark' expand="lg">
-            <div
-                id='col-3-n'
-                className='col-4'
-                style={style}>
+            <div id='col-3-n' className='col-4' style={style}>
                 <NavbarBrand href="/">Habbi Games</NavbarBrand>
                 <NavbarBrand id='col-3-n' href="/">Store</NavbarBrand>
                 {/*Change scss from block dropdown to a full width dropdown*/}
-                <NavDropdown style={{marginLeft:'7%'}}>
+                <NavDropdown style={{
+                    marginLeft: '7%'
+                }}>
                     {categories.map(c => (
                         <NavLink as={Link} to={`/category/${c}`}>{c}</NavLink>
                     ))}
                 </NavDropdown>
             </div>
-            <div className='col-6' style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <div
+                className='col-6'
+                style={{
+                display: 'flex',
+                justifyContent: 'flex-end'
+            }}>
                 <CartWidget/>
             </div>
         </Navbar>
